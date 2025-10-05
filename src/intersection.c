83554441f15f64aaef0cdb7b06926a0dd02c781f
evtish <evevtish@gmail.com>
2025-10-05 16:00:47 +0300

    Simplify a solution of a quadratic equation

--- src/intersection.c
+++ src/intersection.c
@@ -3,12 +3,12 @@
 double hit_sphere(Vec3 center, double radius, Vec3 pixel) {
     Vec3 oc = vec3_sub(center, camera_center);
     double a = vec3_dot(pixel, pixel);
-    double b = -2 * vec3_dot(pixel, oc);
+    double k = vec3_dot(pixel, oc); // b = -2 * k
     double c = vec3_dot(oc, oc) - radius * radius;
-    double discriminant = b * b - 4 * a * c;
+    double discriminant = k * k - a * c;
 
     if (discriminant >= 0)
-        return (-b - sqrt(discriminant)) / (2 * a);
+        return (k + sqrt(discriminant)) / a;
     else
         return -1;
 }
