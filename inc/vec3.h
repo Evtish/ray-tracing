4741577ffab74e3eba97c92d100627615d6a6e9c
evtish <evevtish@gmail.com>
2025-10-06 00:59:57 +0300

    Remove vec3_limit function

--- inc/vec3.h
+++ inc/vec3.h
@@ -12,7 +12,6 @@ double vec3_len(const Vec3 u);
 double vec3_dot(const Vec3 u, const Vec3 v);
 
 Vec3 vec3_normalize(const Vec3 u);
-Vec3 vec3_limit(const Vec3 u, const Vec3 min, const Vec3 max);
 Vec3 vec3_add(const Vec3 u, const Vec3 v);
 Vec3 vec3_sub(const Vec3 u, const Vec3 v);
 Vec3 vec3_mult_n(const Vec3 u, const double n);
